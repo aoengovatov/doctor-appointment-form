b426bd2fd3efbcbb0cdadc214dc2721a63efe127
AlexEngo <a.o.engovatov@yandex.ru>
2024-03-04 16:10:54 +0400

    add Note model & Note controller

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded({extended: false}))
+
 const PORT = config.get("port");
 
 async function start() {
---
+++ server/models/Note.js
@@ -0,0 +1,21 @@
+const { Schema, Model } = require("mongoose");
+const schema = new Schema(
+    {
+        fio: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+            required: true,
+        },
+        problem: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = model("Note", schema);
