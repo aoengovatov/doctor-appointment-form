fb155caa1eb45ce167ea936284821a63b50e57ed
AlexEngo <a.o.engovatov@yandex.ru>
2024-03-04 15:48:59 +0400

    connect db

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 const app = express();
-const PORT = 4000;
+const PORT = config.get("port");
 
 async function start() {
     try {
+        mongoose.connection.once("open", () => {
+            console.log("База данных подключена");
+        });
+        mongoose.connect(config.get("mongoUri"));
+
         app.listen(4000, () => {
             console.log(`Сервер запущен на ${PORT} порту`);
         });
